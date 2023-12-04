706fa785a341cfc25d16dd5e8216fac188aeb978
Vinay Singirikonda <85832194+king1484@users.noreply.github.com>
2023-12-04 12:38:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,23 +4,22 @@ import os
 import uuid
 import streamlit as st
 from pydub import AudioSegment
+from io import BytesIO
 
 st.title("Karaoke")
-url = st.text_area("Enter spotify song URL")
+uploaded_file = st.file_uploader("Upload an audio file (MP3, WAV, or OGG)", type=["mp3", "wav", "ogg"], key="file_uploader")
 button = st.button("Process")
 
-if button:
+if button and uploaded_file:
     with st.spinner("Processing"):
+        # Save the uploaded file
+        audio_content = uploaded_file.read()
         uuid_str = str(uuid.uuid4())
-        cmd = f"spotdl {url} --output audio{uuid_str}"
-        subprocess.run(cmd, shell=True)
-        files = os.listdir(f"audio{uuid_str}")
-        audio_files = [file for file in files if file.endswith(('.mp3', '.wav', '.ogg'))]
-        old_path = os.path.join(f"audio{uuid_str}", audio_files[0])
-        new_path = os.path.join(f"audio{uuid_str}", "audio.mp3")
-        os.rename(old_path, new_path)
-    
-        separator = Separator(f'audio{uuid_str}/audio.mp3', model_name='UVR-MDX-NET-Inst_HQ_3', 
+        audio_path = f"audio{uuid_str}.mp3"  # Save as MP3 for compatibility
+        with open(audio_path, "wb") as f:
+            f.write(audio_content)
+
+        separator = Separator(audio_path, model_name='UVR-MDX-NET-Inst_HQ_3', 
                               use_cuda=True, primary_stem_path=f"music{uuid_str}.wav", output_single_stem="instrumental")
         primary_stem_path = separator.separate()
         
@@ -28,7 +27,7 @@ if button:
         sound.export("output.mp3", format="mp3", bitrate="192k")
     
         os.remove(f"music{uuid_str}.wav")
-        os.remove(f"audio{uuid_str}/audio.mp3")
-        os.rmdir(f"audio{uuid_str}")
+        os.remove(audio_path)
     
         st.audio("output.mp3", format="audio/mp3")
+        st.info("Processing complete. You can upload another file.")
